03abcf393de9ea274d3b87f89454fc8d3d4ab3d1
YazmynS <122718475+YazmynS@users.noreply.github.com>
2024-10-05 19:54:05 +0000

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,22 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "The Game";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Create div
+const div = document.createElement("div");
+
+// Create button
+const button = document.createElement("button");
+button.innerHTML = "Click🔥";
+
+// Append the button to the div container
+div.append(button);
+
+// Append the div container to the app
+app.append(div);
