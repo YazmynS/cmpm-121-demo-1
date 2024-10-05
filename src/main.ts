3c99b58ebca7882dda71051e3489a4670eb40235
YazmynS <122718475+YazmynS@users.noreply.github.com>
2024-10-05 20:15:22 +0000

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,6 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-
-
 // Create button
 const button = document.createElement("button");
 button.innerHTML = "Click🔥";
@@ -18,12 +16,24 @@ app.append(button);
 
 // Create counter
 let count: number = 0;
-const text = document.createElement("h2");
-text.innerText = `Count: ${count}`;
-app.append(text);
+const Text = document.createElement("h2");
+Text.innerText = `Count: ${count}`;
+app.append(Text);
+
+//DisplayText Function
+const DisplayText = () =>{
+    Text.innerText = `Count: ${count}`;
+}
 
 // Increment count on click
 button.addEventListener("click", () => {
-    count++;
-    text.innerText = `Count: ${count}`;
+  count++;
+  DisplayText();
 });
+
+//Increment count per second
+setInterval(() => {
+    count++;
+    DisplayText();
+
+}, 1000);
