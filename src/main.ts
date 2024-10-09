d53daed1a6a476c832b35901494b7a3414d7580b
YazmynS <122718475+YazmynS@users.noreply.github.com>
2024-10-09 20:12:12 +0000

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ app.append(button);
 
 // Create counter
 let count: number = 0;
+let growthRate: number = 0;
 const Text = document.createElement("h2");
 Text.innerText = `Count: ${count}`;
 app.append(Text);
@@ -29,30 +30,7 @@ const DisplayText = () => {
 button.addEventListener("click", () => {
   count++;
   DisplayText();
-  EnableGrowButton(); // Check Grow Button
-});
-
-//Increment count per frame rate
-let frameTime = 0;
-let wholeTime = 0; //Increment count via whole nums
-
-requestAnimationFrame(function animate(time) {
-  //Handle Base case
-  if (frameTime === 0) frameTime = time;
-
-  //Calculate Time (!)
-  wholeTime += (time - frameTime) / 1000;
-
-  //Increment count with whole numbers only
-  while (wholeTime >= 1) {
-    count++;
-    wholeTime -= 1;
-  }
-  //Update values
-  frameTime = time;
-  DisplayText();
-  EnableGrowButton(); // Check Grow Button
-  requestAnimationFrame(animate);
+  EnableGrowButton(); // Enable Grow Button?
 });
 
 // Create Growth Rate button
@@ -67,13 +45,49 @@ const EnableGrowButton = () => {
   if (count >= 10) {
     Growbutton.disabled = false;
   }
+  else{
+    Growbutton.disabled = true;
+  }
 };
 
-//
+//Increment count per frame rate
+let frameTime = 0;
+let wholeTime = 0; //Increment count via whole nums
+let timedGrowth = false;
+
+//Manage the Grow Button Actions with clicked
 Growbutton.addEventListener("click", () => {
   if (count >= 10) {
     count -= 10; // Subtract 10 from the count
     DisplayText();
+    growthRate++;
+    timedGrowth = true;
     EnableGrowButton(); // Check again if button should be disabled after subtraction
   }
 });
+
+requestAnimationFrame(function animate(time) {
+  //Handle Base case
+  if (frameTime === 0) frameTime = time;
+
+  //Calculate Time (!)
+  if(timedGrowth) {
+    wholeTime += (time - frameTime) / 1000;
+
+    //Increment count with whole numbers only
+    while (wholeTime >= 1) {
+      count+= growthRate;
+      wholeTime -= 1;
+    }
+    DisplayText();
+  }
+
+  //Update values
+  frameTime = time;
+  EnableGrowButton(); // Check Grow Button
+  requestAnimationFrame(animate);
+});
+
+
+
+
