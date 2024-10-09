da8170ca8c8b2f6316afb495f3453b365d5357e9
YazmynS <122718475+YazmynS@users.noreply.github.com>
2024-10-09 19:45:06 +0000

    Step 5: Feature Enable/Disable Grow Button Added

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// Create button
+// Create Click button
 const button = document.createElement("button");
 button.innerHTML = "Click🔥";
 app.append(button);
@@ -29,26 +29,51 @@ const DisplayText = () => {
 button.addEventListener("click", () => {
   count++;
   DisplayText();
+  EnableGrowButton(); // Check Grow Button
 });
 
 //Increment count per frame rate
 let frameTime = 0;
-let wholeTime = 0;  //Increment count via whole nums
+let wholeTime = 0; //Increment count via whole nums
 
-requestAnimationFrame(function animate(time){
+requestAnimationFrame(function animate(time) {
   //Handle Base case
   if (frameTime === 0) frameTime = time;
-  
+
   //Calculate Time (!)
   wholeTime += (time - frameTime) / 1000;
-  
+
   //Increment count with whole numbers only
-  while (wholeTime >= 1){ 
-    count++; 
+  while (wholeTime >= 1) {
+    count++;
     wholeTime -= 1;
   }
   //Update values
   frameTime = time;
   DisplayText();
+  EnableGrowButton(); // Check Grow Button
   requestAnimationFrame(animate);
-})
+});
+
+// Create Growth Rate button
+const Growbutton = document.createElement("button");
+Growbutton.innerHTML = "Grow (10 clicks req.)";
+Growbutton.id = "growButton";
+Growbutton.disabled = true;
+app.append(Growbutton);
+
+//Enable Grow Button
+const EnableGrowButton = () => {
+  if (count >= 10) {
+    Growbutton.disabled = false;
+  }
+};
+
+//
+Growbutton.addEventListener("click", () => {
+  if (count >= 10) {
+    count -= 10; // Subtract 10 from the count
+    DisplayText();
+    EnableGrowButton(); // Check again if button should be disabled after subtraction
+  }
+});
